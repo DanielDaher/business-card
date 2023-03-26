debb34e00eea59807dd0d7add6ec830452689d59
Daniel <danielfdoficial@gmail.com>
2023-03-26 01:01:37 -0300

    refactor inputs and add style to page

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,20 +1,42 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import '../Styles/Form.css';
+
 
 function Form() {
-  const [count, setCount] = useState(0);
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    linkedinURL: '',
+    githubURL: ''
+  });
 
   const genereateQRCodeImage = (e) => {
     e.preventDefault();
     alert('função disparada')
   };
 
+  const handleInputChange = (info, event) => setUserInfo({ ...userInfo, [info]: event.target.value });
+
   return (
-    <form className='form' onSubmit={(e) => genereateQRCodeImage(e)}>
-      <input type="text" placeholder='Name'/>
-      <input type="text" placeholder='Linkedin URL'/>
-      <input type="text" placeholder='Github URL'/>
-      <button>Generate Image</button>
-    </form>
+    <div className='card'>
+      <h3>QR CODE IMAGE GENERATOR</h3>
+      <form onSubmit={(e) => genereateQRCodeImage(e)}>
+        {Object.keys(userInfo).map((info) => {
+          const placeholderValue = { name: 'Name', linkedinURL: 'Linkedin URL', githubURL: 'Github URL' };
+          const title = 'Type your name';
+          return (
+            <input
+              type="text"
+              key={info}
+              value={userInfo[info]}
+              placeholder={placeholderValue[info]}
+              title={info === 'name' ? title : null}
+              onChange={(event) => handleInputChange(info, event)}
+            />
+        )
+        })}
+        <button title="Make a new QR CODE with your informations">Generate Image</button>
+      </form>
+    </div>
   )
 }
 
