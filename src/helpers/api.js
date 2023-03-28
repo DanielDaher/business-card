774595e6c890cce22dbe9796b5349e647214b636
Daniel <danielfdoficial@gmail.com>
2023-03-28 17:36:27 -0300

    add env of backend url

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import { isValidRequest } from "./utils";
-// require('dotenv').config();
-const API_BASE_URL = /* `${process.env.REACT_APP_API_URL}` ||  */'http://localhost:3000';
+/// require('dotenv').config();
+const API_BASE_URL = `${process.env.REACT_APP_API_URL}` || 'http://localhost:3000';
 
 export const registerNewCard = async (userInfo) => {
   const validRequest = isValidRequest(userInfo);
